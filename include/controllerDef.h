c5e243f74360aac13381d58c31702650e19d8635
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-01-18 10:23:53 -0800

    Debug linker errors
    This is a large commitbut the basic gist is
    that the global variables for the motor and controller needed
    the extern keyword.

--- include/controllerDef.h
+++ include/controllerDef.h
@@ -1,5 +1,5 @@
 #ifndef TOWER_TAKEOVER_CONTROLLER_DEFINITIONS_H
 #define TOWER_TAKEOVER_CONTROLLER_DEFINITIONS_H
 #include "vex.h"
-vex::controller Controller1 = vex::controller();
+extern vex::controller Controller1;
 #endif
--- include/motorDef.h
+++ include/motorDef.h
@@ -1,14 +1,16 @@
 #ifndef TOWER_TAKEOVER_MOTOR_DEFINITIONS_H
 #define TOWER_TAKEOVER_MOTOR_DEFINITIONS_H
 #include "vex.h"
-vex::motor LeftMotor = vex::motor(vex::PORT1, false);
-vex::motor RightMotor = vex::motor(vex::PORT2, true);      
-vex::motor RightClaw = vex::motor(vex::PORT5, false);
-vex::motor LeftClaw = vex::motor(vex::PORT6, true);
-vex::motor Ramp = vex::motor(vex::PORT9, true);
-vex::motor pusher = vex::motor(vex::PORT3,false);
-vex::motor pusher2 = vex::motor(vex::PORT10,true);
-vex::motor CupL = vex::motor(vex::PORT11,false);
-vex::motor CupR = vex::motor(vex::PORT20,true);
-vex::motor Lifter = vex::motor(vex::PORT19,false);
+extern vex::motor LeftMotor;
+extern vex::motor RightMotor;       
+extern vex::motor RightClaw;
+extern vex::motor LeftClaw; 
+extern vex::motor Ramp; 
+extern vex::motor pusher;
+extern vex::motor pusher2;
+extern vex::motor CupL;
+extern vex::motor CupR;
+extern vex::motor Lifter;
+
 #endif
+
