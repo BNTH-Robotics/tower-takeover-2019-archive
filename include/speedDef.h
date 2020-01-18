5332d91bdff34280f6ed16bafd9eac239e4e6f34
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-01-17 20:14:44 -0500

    Make speed definitions constant for faster access

--- include/speedDef.h
+++ include/speedDef.h
@@ -1,16 +1,16 @@
 #ifndef TOWER_TAKEOVER_SPEED_DEFINITIONZ_H
 #define TOWER_TAKEOVER_SPEED_DEFINITIONZ_H
-int ASpeedPCT {200};
-int fwd {100};
-int BSpeedPCT {20};
-int CSpeedPCT{200};
-int DSpeedPCT{50};
-int ESpeedPCT{100};
-int FSpeedPCT{75};
-int PSpeedPCT{24};
-int HSpeedPCT{100};
-int ISpeedPCT{50};
-int JSpeedPCT{50};
-int KSpeedPCT{50};
-int LSpeedPCT{50};
+constexpr int ASpeedPCT {200};
+constexpr int fwd {100};
+constexpr int BSpeedPCT {20};
+constexpr int CSpeedPCT{200};
+constexpr int DSpeedPCT{50};
+constexpr int ESpeedPCT{100};
+constexpr int FSpeedPCT{75};
+constexpr int PSpeedPCT{24};
+constexpr int HSpeedPCT{100};
+constexpr int ISpeedPCT{50};
+constexpr int JSpeedPCT{50};
+constexpr int KSpeedPCT{50};
+constexpr int LSpeedPCT{50};
 #endif
