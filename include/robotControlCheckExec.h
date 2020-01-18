491a974469fece768ee30216c5015cd4ce98ed76
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-01-17 20:49:54 -0500

    Inline optimization for controlCheck functions

--- include/robotControlCheckExec.h
+++ include/robotControlCheckExec.h
@@ -2,11 +2,11 @@
 #define TOWER_TAKEOVER_ROBOT_CONTROL_CHECK_EXEC_H
 namespace control
 {
-inline void joystickPosition();
-inline void buttonAbuttonBclaw();
-inline void buttonR1buttonR2();
-inline void buttonL1buttonL2();
-inline void buttonUpbuttonDown();
-inline void buttonRight();
+    inline void joystickPosition();
+    inline void buttonAbuttonBclaw();
+    inline void buttonR1buttonR2();
+    inline void buttonL1buttonL2();
+    inline void buttonUpbuttonDown();
+    inline void buttonRight();
 }
 #endif
